b75157eae70ba7f6847cf85053db46bfa5686399
ng1610 <74397928+ng1610@users.noreply.github.com>
2021-04-14 17:36:57 +0530

    Add files via upload

---
+++ chitchat_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCHn62aprkj2CPpUAY9Lne4lge_N5JuygY",
+    authDomain: "chitchat-67bde.firebaseapp.com",
+    databaseURL: "https://chitchat-67bde-default-rtdb.firebaseio.com",
+    projectId: "chitchat-67bde",
+    storageBucket: "chitchat-67bde.appspot.com",
+    messagingSenderId: "788598319431",
+    appId: "1:788598319431:web:69bb6b7fb633c77324c462",
+    measurementId: "G-4120FVXLS1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
